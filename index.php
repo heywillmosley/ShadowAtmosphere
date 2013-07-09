7933f2250014953878b8c809f9d1cd472a69405c
William Mosley <will@livesuperamazing.com>
2013-07-09 06:16:59 -0400

    Bugfix: Fixed broken absolute urls

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php # Prevents direct access to config.inc.php 
+
+/**
+ * Shadow
+ *
+ * An open source application development framework that streamlines
+ * responsive e-ecommerce development for php 5.0.0 or newer
+ *
+ * @package        Shadow
+ * @author         Super Amazing
+ * @copyright      Copyright (c) 2010 - 2013, Super Amazing
+ * @license        
+ * @link           http://shadow.livesuperamazing.com
+ * @since          Version 0.1.1 s8
+ * -----------------------------------------------------------------
+ *
+ * This page loads Shadow Architecture
+ */
+ 
+// --------------------------------------------------------------------------------
+
+/**
+ * Require the index from Shadow before anything is loaded
+ * If Your Shadow Directory is named different, pleasde change 'Shadow/index.php'
+ * to the correct path
+ */
+ 	require_once 'Shadow/index.php';
